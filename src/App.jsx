bf22e7c69f158f27f8a9d7a29ba2f7e98437a7b6
RiyaSreeParigi <riyasreeparigi@gmail.com>
2026-07-28 20:59:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Pendant from "./components/Pendant";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -12,6 +13,8 @@ function App() {
       <Navbar />
 
       <Hero />
+
+      <Pendant />
 
       <Features />
 
