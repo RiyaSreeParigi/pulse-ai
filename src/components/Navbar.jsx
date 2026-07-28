3169ab8e81176f3d56ba02238ec36ab9db027c41
RiyaSreeParigi <riyasreeparigi@gmail.com>
2026-07-28 20:42:54 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <h1 className="logo">
+        PULSE AI
+      </h1>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Features</li>
+        <li>Dashboard</li>
+        <li>Contact</li>
+      </ul>
+
+    </nav>
+  );
+}
+
+export default Navbar;
