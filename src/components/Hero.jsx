750f2e54ec7f0abe093a3a25b7fb3c6f8f2dcc45
RiyaSreeParigi <riyasreeparigi@gmail.com>
2026-07-28 20:43:56 +0530

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+function Hero() {
+  return (
+    <section className="hero">
+
+      <div className="hero-content">
+
+        <h2>
+          Your AI Health Companion
+        </h2>
+
+        <p>
+          PULSE AI combines smart health monitoring,
+          artificial intelligence, and real-time insights
+          to help you understand your body's signals.
+        </p>
+
+        <button className="btn">
+          Explore PULSE
+        </button>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Hero;
